dbc8a63703b63f957a7a477e97afb73af69a2022
comphand <composershandle@gmail.com>
2025-09-30 20:46:41 +0900

    3x3 to 4x4

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function Square({value,onSquareClick}){
 
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill('blank'));
+  const [squares, setSquares] = useState(Array(16).fill('blank'));
 
   function handleClick(i){
     if (squares[i] || calculateWinner(squares)){
@@ -32,16 +32,25 @@ export default function Board() {
         <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
         <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
         <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
         <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
         <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-      </div>
-      <div className="board-row">
         <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
         <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+      </div>
+      <div className="board-row">
         <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+        <Square value={squares[9]} onSquareClick={() => handleClick(9)}/>
+        <Square value={squares[10]} onSquareClick={() => handleClick(10)}/>
+        <Square value={squares[11]} onSquareClick={() => handleClick(11)}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[12]} onSquareClick={() => handleClick(12)}/>
+        <Square value={squares[13]} onSquareClick={() => handleClick(13)}/>
+        <Square value={squares[14]} onSquareClick={() => handleClick(14)}/>
+        <Square value={squares[15]} onSquareClick={() => handleClick(15)}/>
       </div>
     </>
   )
